6ea303eb4944147c236c184d5b126d1d6e7262d6
lec.jiang <lec.jiang.dev@gmail.com>
2025-06-23 02:42:30 +0900

    xml

---
+++ files/hadoop-env.sh
@@ -0,0 +1,8 @@
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-arm64
+
+export HDFS_DATANODE_USER=hdfs
+export HDFS_DATANODE_SECURE_USER=hdfs
+
+export JSVC_HOME=/usr/bin
+
+export HADOOP_OPTS="-Djava.net.preferIPv4Stack=true"
